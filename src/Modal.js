81e245c42a84fb14dd4816a798f94bff55c64b15
solo5star <solo_5@naver.com>
2023-05-02 12:00:40 +0900

    feat: 모달 구현 추가

---
+++ src/Modal.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export const MyModal = ({ trigger, children }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      {React.cloneElement(trigger, { onClick: handleOpenModal })}
+      {isModalOpen && (
+        <div className="modal-overlay" onClick={handleCloseModal}>
+          <div className="modal" onClick={(e) => e.stopPropagation()}>
+            {children}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
